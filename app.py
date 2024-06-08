84c33dd26c0bac4699b6deeac565b9714a335f23
BeomSu <48756063+Seobeomsu@users.noreply.github.com>
2024-06-08 22:43:33 +0900

    [Add]:app.py_server

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import torch
+from PIL import Image
+from io import BytesIO
+
+app = FastAPI()
+
+# 모델 로드
+model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
+
+def transform_image(image_bytes):
+    img = Image.open(BytesIO(image_bytes))
+    return img
+
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    image = await file.read()
+    img = transform_image(image)
+    results = model(img)
+    return JSONResponse(results.pandas().xyxy[0].to_dict(orient="records"))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
